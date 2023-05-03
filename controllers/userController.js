c9e6daa38864bc4e3febb6a54c7339df77c0af68
Carechiga <Carechiga43@gmail.com>
2023-05-03 11:15:41 -0700

    minor fixes to delete routes to make sure errors dont return

--- controllers/userController.js
+++ controllers/userController.js
@@ -43,7 +43,6 @@ module.exports = {
             return res.status(404).json({ message: 'No user with that ID' });
         }
 
-        await Application.deleteMany({ _id: { $in: user.thoughts } });
         res.json({ message: 'User and associated thoughts deleted!' })
         } catch (err) {
         res.status(500).json(err);
